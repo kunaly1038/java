82169f6571b3a801da4c624942d8ce01c83a3bed
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-08-13 00:45:54 +0530

    Create secondLargestArray.java

---
+++ Basic/secondLargestArray.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Notes {
+    static Scanner sc = new Scanner(System.in);
+    public static int  removeDuplicate(int [] arr)
+    {
+          if (arr.length < 2)
+        {
+            System.out.print(" Invalid Input ");
+            return 0;
+        }
+      
+        Arrays.sort(arr); 
+        int largest=0, second=0;
+        for(int i=0 ; i<arr.length ; i++)
+        {
+            if(arr[i] > largest)
+            {
+                second = largest;
+                largest = arr[i];
+            }
+        }
+        return second;
+    } 
+    public static int[] fillArray(int [] arr)
+    {
+        for(int i =0 ; i<arr.length; i++)
+        {
+            System.out.println("Enter the Element into the Array :");
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+       System.out.println("Enter the size of Array");
+       int size = sc.nextInt();
+       int [] arr = new int[size];
+       int []arr2 =fillArray(arr);
+       
+       System.out.println(removeDuplicate(arr2));
+    }    
+}
