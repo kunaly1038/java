ab197d858179b9a551d30dd6d3ad3c226e7ce29b
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-08-25 00:29:18 +0530

    Update linked_list_1.java

--- Data Structure/Linked List/linked_list_1.java
+++ Data Structure/Linked List/linked_list_1.java
@@ -1,17 +1,12 @@
-package linkedlist;
-
-class LinkedList
-{
-    Node head;
-    static class Node
-    {
-        int data;
-        Node next;
-        
-        Node(int d)
-        {
-            data = d;
-            next = null;
-        }
+class Node{
+    int data;
+    Node next;
+    public Node(int d){
+        data = d;
+        next = null;
     }
+}
+
+class Linkedlist{
+    Node head;
 }
