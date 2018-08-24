c52602841e3a5b95eb351b0641f0128a3814e912
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-08-25 01:29:41 +0530

    Create linked_list_atPosition.java

---
+++ Data Structure/Linked List/linked_list_atPosition.java
@@ -0,0 +1,58 @@
+class Linkedlist{
+   Node head;
+    class Node{
+       int data;
+       Node next;
+      
+    }
+    public void insertList(int data){
+        Node node =new Node();
+        node.data = data;
+        if(head ==  null){
+            head = node;
+        }
+        else{
+            Node n = head;
+            while(n.next!=null){
+                n = n.next;
+            }
+            n.next = node;
+        }
+    }
+    public void insertAtBegin(int data){
+        Node node = new Node(); 
+        node.data = data;
+        node.next = head;
+        head = node;
+    }
+    public void insertAtPost(int index, int data){
+        Node node =  new Node();
+        Node n;
+        node.data = data;
+        n = head;
+        for(int i=0; i<index-1; i++){
+            n = n.next;
+        }
+        node.next = n.next;
+        n.next = node;
+    }
+    
+    public void printList(){
+        Node n = head;
+        while(n!=null){
+            System.out.print(n.data+" ");
+            n = n.next;
+        }
+    }
+    public static void main(String [] args){
+        Linkedlist list = new Linkedlist();
+        list.insertAtBegin(25);
+        list.insertAtBegin(18);
+        list.insertAtBegin(45);
+        list.insertAtBegin(12);
+        list.insertAtBegin(5);
+        list.insertAtPost(5, 9);
+        list.printList();
+    }
+    
+}
