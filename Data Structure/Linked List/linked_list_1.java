ffb5cbfd546dbf77fe4ebb824b4f8763c75d5777
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-08-25 00:05:08 +0530

    Create linked_list_1.java

---
+++ Data Structure/Linked List/linked_list_1.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+class LinkedList
+{
+    Node head;
+    static class Node
+    {
+        int data;
+        Node next;
+        
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
