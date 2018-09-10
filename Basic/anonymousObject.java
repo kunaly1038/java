4ad75bd8a9324664f5a8e4d3efef5bd49c9540f2
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-09-11 01:17:23 +0530

    Create anonymousObject.java

---
+++ Basic/anonymousObject.java
@@ -0,0 +1,13 @@
+
+package study;
+
+public class Study {
+
+    public void fun(){
+        System.out.println("hello jaava");
+    }
+    public static void main(String[] args) {
+        new Study().fun();
+    }
+    
+}
