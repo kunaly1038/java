32c794eb7b4d215cfd24b6dd1b699e17867ac184
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-09-20 22:08:09 +0530

    Create createNode.java

---
+++ Data Structure/Binary Search Tree/createNode.java
@@ -0,0 +1,10 @@
+//create node in java of binary search tree
+public class Queue{
+    class bst{
+        int data;
+        bst left, right;
+        public bst(int data){
+            this.data = data;
+            left = right = null;
+        }
+    }
