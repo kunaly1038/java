c188307eadce6c941c326d9e59e9fa9781aac40f
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-09-21 18:49:54 +0530

    Update power.java

--- Basic/power.java
+++ Basic/power.java
@@ -3,7 +3,11 @@ import java.util.*;
 public class Power{
     static Scanner sc = new Scanner(System.in);
     public static void main(String... args){
-        int x=5, y=3, result, num;
+        int x, y, result, num;
+        System.out.println("Enter the number :");
+        x = sc.nextInt();
+        System.out.println("Enter the power :");
+        y = sc.nextInt();
         result = num = x;
         for(int i=1; i<y; i++){
             for(int j=1; j<x; j++){
